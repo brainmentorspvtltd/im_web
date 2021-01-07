06b39ecc27956b74fd375b297bcfa85183c5a240
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-01-07 16:54:31 +0530

    Add files via upload

---
+++ v4/expense-crud/scripts/models/expense.js
@@ -0,0 +1,23 @@
+//Data Model
+/*
+function createExpense(id, name, price, color, date, remarks){
+    // literal
+const expenseObject = {
+   id, name, price, color, date, remarks
+};
+return expenseObject;
+}*/
+// function constructor
+function Expense(id , name , price, color, date, remarks){
+    this.id = id;
+    this.name = name;
+    this.price = price;
+
+    this.date = date;
+    this.color = color;
+    this.remarks = remarks;
+    this.markForDelete = false;
+}
+Expense.prototype.toggleMark = function(){
+    this.markForDelete = !this.markForDelete;
+}
